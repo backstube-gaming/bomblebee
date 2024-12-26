11b758ed8b05916478011467bc1b4f35fbb3eb7f
backstube-gaming <official.backstube.gaming@gmail.com>
2024-12-26 13:18:06 +0100

    initial commit

---
+++ src/client/java/net/backstube/bomblebee/BomblebeeClient.java
@@ -0,0 +1,10 @@
+package net.backstube.bomblebee;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BomblebeeClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
